33ea299eae591ff08fbf021a0c3c3f4d4dec32f4
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-12-19 21:46:48 +0200

    Add dataclasses to dependencies for python3.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/JonatanMartens/eventipy",
     packages=setuptools.find_packages(exclude=("tests",)),
-    install_requires=[],
+    install_requires=["dataclasses==0.6"],
     exclude=["*test.py", "tests"],
     keywords="event pubsub events",
     license="MIT",
